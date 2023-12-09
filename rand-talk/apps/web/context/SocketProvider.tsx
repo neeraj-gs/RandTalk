8061f0eaa4bffaec75a6f9928b25345adb40d500
neeraj-gs <gsneeraj05@gmail.com>
2023-12-09 18:54:50 +0530

    Startef working ont he frontend a dn creted a context for all our react app for modularizing the app

---
+++ rand-talk/apps/web/context/SocketProvider.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+//frontend 
+import React from 'react'
+
+interface SocketProviderProps {
+    children?:React.ReactNode
+}
+
+
+const SocketContext=React.createContext(null);
+
+export const SocketProvider: React.FC<SocketProviderProps> = ({children})=>{
+    return(
+        <SocketContext.Provider value={null}>
+            {children}
+        </SocketContext.Provider>
+    )
+}
+
+
