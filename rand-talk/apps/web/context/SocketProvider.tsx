4718aa74e19df7ad405465be2ec3c4d4d59ef78e
neeraj-gs <gsneeraj05@gmail.com>
2023-12-09 18:58:28 +0530

    Started creating context that gives diffrent events and need ot implement those events

--- rand-talk/apps/web/context/SocketProvider.tsx
+++ rand-talk/apps/web/context/SocketProvider.tsx
@@ -1,18 +1,29 @@
 'use client'
 
 //frontend 
-import React from 'react'
+import React, { useCallback } from 'react'
 
 interface SocketProviderProps {
     children?:React.ReactNode
 }
 
 
-const SocketContext=React.createContext(null);
+interface ISocketContext {
+    sendMsg: (msg:string) => any;
+}
+
+
+const SocketContext=React.createContext<ISocketContext | null>((null));
 
 export const SocketProvider: React.FC<SocketProviderProps> = ({children})=>{
+
+    const sendMsg: ISocketContext['sendMsg'] = useCallback((msg)=>{
+        console.log("SEND MESSage")
+    },[])
+
+
     return(
-        <SocketContext.Provider value={null}>
+        <SocketContext.Provider value={{sendMsg}}>
             {children}
         </SocketContext.Provider>
     )
